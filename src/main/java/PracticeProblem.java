03e64c97da65b624429ae56573f8e54b06d2dac7
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-12 14:36:45 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,52 @@
-public class PracticeProblem {
+public class PracticeProblem{
 
-	public static void main(String args[]) {
-
+	public static String evenOrOdd(int num){
+		if((num&1) == 0)
+			return "Even";
+		return "Odd";
 	}
 
-	//Write your functions here
+	public static String teacherOrStudent(String name){
+		if(name.equals("Kalisz"))
+			return "Teacher";
+		return "Student";
+	}
 
+	public static int fartherFromZero(int num){
+		if(num > 0)
+			return num + 5;
+		if(num < 0)
+			return num - 5;
+		return 0;
+	}
+
+	public static String isFive(int num){
+		if(num == 5)
+			return "The number is Five";
+		else
+			return "The number is not Five";
+	}
+	
+	public static String positiveOrNegative(double num){
+		if(num > 0)
+			return "Positive";
+		else
+			return "Negative";
+	}
+
+	public static String highOrLow(int num){
+		if(num > 100)
+			return "High";
+		else
+			return "Low";
+	}
+
+	public static String isHello(String word){
+		if(word.equals("Hello"))
+			return "The word is Hello";
+		else
+			return "The word is not Hello";
+	}
+
+	public static void main(String args[]){}
 }
